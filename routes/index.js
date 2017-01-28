98865f429aa00ee96b438cfcdf224b3ee30ad934
Alvin James Bellero <alvinb@Zyluns-MacBook-Pro.local>
2017-01-28 15:20:25 +0800

    Updated route and view for index

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'User Management System' });
+  res.render('index', { title: 'User Management System v 1.0' });
 });
 
 module.exports = router;
