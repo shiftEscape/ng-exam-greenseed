773c12d518469bfabef2837f190d5e9d06d72ed6
Alvin James Bellero <ajames.bellero@gmail.com>
2017-01-30 16:47:22 +0800

    Modified routes for user and authentication

--- routes/api/authenticate.js
+++ routes/api/authenticate.js
@@ -1,25 +1,25 @@
 var User = require('../../models/user');
+var crypto = require('crypto')
 var jwt  = require('jsonwebtoken');
 
 module.exports = function(app, middleware) {
 
     authenticateUser = function (req, res) {
-        User.findOne({
+        User.model.findOne({
             username: req.body.username
         }, function(err, user) {
 
-            if (err) throw err;
-
-            if (!user) {
-                res.json({ success: false, message: 'Authentication failed. User not found.' });
+            if (err) {
+                res.status(400).json({ success: false, message: err });
+            } else if (!user) {
+                res.status(401).json({ success: false, message: 'Authentication failed. User not found.' });
             } else if (user) {
-                
-                if (user.password != req.body.password) {
-                    res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+                if (!user.validatePassword(req.body.password)) {
+                    res.status(401).json({ success: false, message: 'Authentication failed. Wrong password.' });
                 } else {
 
                     var token = jwt.sign(user, app.get('appSecret'), {
-                        expiresIn: 60*60*24
+                        expiresIn: app.get('tokenExpiry')
                     });
 
                     res.json({ success: true, token: token });
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,17 +1,34 @@
 var User = require('../../models/user');
 
 module.exports = function(app, middleware) {
-    
+
+    createUser = function(req, res) {
+        User.createUser(req.body, function(err, createdUser) {
+            if(err) {
+                res.status(400).json({success: false, message: err})
+            } else {
+                res.status(201).json({success: true, _id: createdUser._id})
+            }
+        });
+    };
+
     getAllUsers = function(req, res) {
-        User.find({}, function(err, users) {
+        User.model.find({}, function(err, users) {
             res.json(users);
         });
     };
 
     getUser = function(req, res) {
-        var id = req.params.id;
+        User.model.find({_id: req.params.id}, function(err, user) {
+            if(err) {
+                res.status(400).json({success: false, message: err})
+            } else {
+                res.status(200).json({success: true, data: user});
+            }
+        });
     };
     
+    app.post('/api/users', [middleware.auth.requireToken], createUser);
     app.get('/api/users', [middleware.auth.requireToken], getAllUsers);
     app.get('/api/users/:id', [middleware.auth.requireToken], getUser);
 };
