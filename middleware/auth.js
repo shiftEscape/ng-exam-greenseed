ec3f91c6941f8d014b5b6130bdf8fd85475d0a62
Alvin James Bellero <ajames.bellero@gmail.com>
2017-01-30 16:48:13 +0800

    Modified auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ var config = require('../config');
 
 module.exports.requireToken = function(req, res, next) {
 
-  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  var token = req.headers['x-access-token'];
 
   if (token) {
     jwt.verify(token, config.secret, function(err, decoded) {      
@@ -19,7 +19,7 @@ module.exports.requireToken = function(req, res, next) {
 
     return res.status(403).send({ 
         success: false, 
-        message: 'No token provided.' 
+        message: 'No token provided.'
     });
     
   }
